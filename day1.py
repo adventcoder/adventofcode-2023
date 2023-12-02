02768beaf51f8ec9b7db0bd508bedb3dd44dce43
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-02 19:41:40 +0000

    rename

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 import aoc
 
-digits_as_words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 @aoc.puzzle()
 def part1(doc):
@@ -15,11 +15,11 @@ def calibration_value(s):
     return int(digits[0] + digits[-1])
 
 def real_calibration_value(s):
-    for d1, word1 in enumerate(digits_as_words):
-        for d2, word2 in enumerate(digits_as_words):
+    for d1, word1 in enumerate(words):
+        for d2, word2 in enumerate(words):
             if word1[-1] == word2[0]:
                 s = s.replace(word1 + word2[1:], str(d1) + str(d2))
-    for d, word in enumerate(digits_as_words):
+    for d, word in enumerate(words):
         s = s.replace(word, str(d))
     return calibration_value(s)
 
