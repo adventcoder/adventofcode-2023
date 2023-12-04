c696dd49fb7f9b2b05ee651ae2cbceabcc3efc42
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-04 18:07:37 +0000

    optimized

--- day1.py
+++ day1.py
@@ -1,27 +1,30 @@
 import aoc
+import re
 
-words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+forward_regex = re.compile('[0-9]|' + '|'.join(re.escape(name) for name in names))
+backward_regex = re.compile('[0-9]|' + '|'.join(re.escape(name[::-1]) for name in names))
 
 @aoc.puzzle()
 def part1(inp):
     return sum(calibration_value(s) for s in inp.splitlines())
 
+def calibration_value(s):
+    first_digit = next(c for c in s if c.isdigit())
+    last_digit = next(c for c in reversed(s) if c.isdigit())
+    return int(first_digit + last_digit)
+
 @aoc.puzzle()
 def part2(inp):
     return sum(real_calibration_value(s) for s in inp.splitlines())
 
-def calibration_value(s):
-    ds = [c for c in s if c.isdigit()]
-    return int(ds[0] + ds[-1])
-
 def real_calibration_value(s):
-    for d1, word1 in enumerate(words):
-        for d2, word2 in enumerate(words):
-            if word1[-1] == word2[0]:
-                s = s.replace(word1 + word2[1:], str(d1) + str(d2))
-    for d, word in enumerate(words):
-        s = s.replace(word, str(d))
-    return calibration_value(s)
+    first_digit = digit(forward_regex.search(s).group())
+    last_digit = digit(backward_regex.search(s[::-1]).group()[::-1])
+    return int(first_digit + last_digit)
+
+def digit(s):
+    return s if s.isdigit() else str(names.index(s))
 
 if __name__ == '__main__':
     aoc.main()
