ecaee12df89071122aed3786d8be5a0b2b593458
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-08 18:47:25 +0000

    meh

--- day1.py
+++ day1.py
@@ -1,30 +1,28 @@
 import aoc
 import re
 
-names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-forward_regex = re.compile('[0-9]|' + '|'.join(re.escape(name) for name in names))
-backward_regex = re.compile('[0-9]|' + '|'.join(re.escape(name[::-1]) for name in names))
+digit_names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 @aoc.puzzle()
 def part1(inp):
-    return sum(calibration_value(s) for s in inp.splitlines())
+    return sum(int(find_digit(s) + find_digit(reversed(s))) for s in inp.splitlines())
 
-def calibration_value(s):
-    first_digit = next(c for c in s if c.isdigit())
-    last_digit = next(c for c in reversed(s) if c.isdigit())
-    return int(first_digit + last_digit)
+def find_digit(s):
+    return next(filter(str.isdigit, s))
 
 @aoc.puzzle()
 def part2(inp):
-    return sum(real_calibration_value(s) for s in inp.splitlines())
+    lines = inp.splitlines()
+    first_digits = map(digit_finder(digit_names), lines)
+    last_digits = map(digit_finder([name[::-1] for name in digit_names]), (line[::-1] for line in lines))
+    return sum(int(a + b) for a, b in zip(first_digits, last_digits))
 
-def real_calibration_value(s):
-    first_digit = digit(forward_regex.search(s).group())
-    last_digit = digit(backward_regex.search(s[::-1]).group()[::-1])
-    return int(first_digit + last_digit)
-
-def digit(s):
-    return s if s.isdigit() else str(names.index(s))
+def digit_finder(names):
+    regex = re.compile('|'.join([r'[0-9]'] + [re.escape(name) for name in names]))
+    def find_digit(s):
+        group = regex.search(s).group()
+        return group if group.isdigit() else str(names.index(group))
+    return find_digit
 
 if __name__ == '__main__':
     aoc.main()
