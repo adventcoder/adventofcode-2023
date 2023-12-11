b86a4c2f38ffc650516f4c6666743d3b377a9824
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-11 21:08:06 +0000

    blah

--- day10.py
+++ day10.py
@@ -1,81 +1,64 @@
 import aoc
 
-#TODO: rewrite to shoelace formula
-
-#   3
-# 1 + 2
 #   0
-dx = [0, -1, 1,  0]
-dy = [1,  0, 0, -1]
+# 1 + 2
+#   3
+dx = [ 0, -1, 1, 0]
+dy = [-1,  0, 0, 1]
 
 dirs = {
-    '7': (0, 1),
-    'F': (0, 2),
+    'J': (0, 1),
+    'L': (0, 2),
     '|': (0, 3),
     '-': (1, 2),
-    'J': (1, 3),
-    'L': (2, 3)
+    '7': (1, 3),
+    'F': (2, 3)
 }
 
-def move(x, y, d):
-    return x + dx[d], y + dy[d]
+dd = { c: sum(dirs[c]) - 3 for c in dirs }
 
-def turn(d, c):
-    d0, d1 = dirs[c]
-    return d + d0 + d1 - 3
+def start(grid):
+    for y, row in enumerate(grid):
+        for x, c in enumerate(row):
+            if c == 'S':
+                return x, y, start_dir(grid, x, y)
 
-def connects(grid, x, y, d):
-    nx = x + dx[d]
-    ny = y + dy[d]
-    return 0 <= ny < len(grid) and 0 <= nx < len(grid[ny]) and 3 - d in dirs[grid[ny][nx]]
+def start_dir(grid, x, y):
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= ny < len(grid) and 0 <= nx < len(grid[ny]) and 3 - d in dirs[grid[ny][nx]]:
+            return d
 
 @aoc.puzzle()
 def part1(inp):
     grid = inp.splitlines()
-    Sx, Sy = next((x, y) for y, line in enumerate(grid) for x, c in enumerate(line) if c == 'S')
-    d = next(d for d in range(4) if connects(grid, Sx, Sy, d))
-    x, y = move(Sx, Sy, d)
-    n = 1
-    while x != Sx or y != Sy:
-        d = turn(d, grid[y][x])
-        x, y = move(x, y, d)
+    x, y, d = start(grid)
+    n = 0
+    while True:
+        x += dx[d]
+        y += dy[d]
         n += 1
+        if grid[y][x] == 'S':
+            break
+        d += dd[grid[y][x]]
     return n // 2
 
 @aoc.puzzle()
-def part2_old(inp):
+def part2(inp):
     grid = inp.splitlines()
-    loop = [[False] * len(line) for line in grid]
-    Sx, Sy = next((x, y) for y, line in enumerate(grid) for x, c in enumerate(line) if c == 'S')
-    c = next(c for c in dirs.keys() if all(connects(grid, Sx, Sy, d) for d in dirs[c]))
-    grid[Sy] = grid[Sy][:Sx] + c + grid[Sy][Sx+1:]
-    loop[Sy][Sx] = True
-    d = dirs[c][0]
-    x, y = move(Sx, Sy, d)
-    while x != Sx or y != Sy:
-        loop[y][x] = True
-        d = turn(d, grid[y][x])
-        x, y = move(x, y, d)
+    x, y, d = start(grid)
     area = 0
-    for y, row in enumerate(grid):
-        inside = 0
-        x = 0
-        while x < len(row):
-            c = row[x]
-            if loop[y][x]:
-                if c == '|':
-                    inside ^= 1
-                else:
-                    assert c in 'LF'
-                    x += 1
-                    while row[x] == '-':
-                        x += 1
-                    if (c == 'F' and row[x] == 'J') or (c == 'L' and row[x] == '7'):
-                        inside ^= 1
-            else:
-                area += inside
-            x += 1
-    return area
+    edge = 0
+    while True:
+        px, x = x, x + dx[d]
+        py, y = y, y + dy[d]
+        area += px*y - x*py
+        edge += 1
+        if grid[y][x] == 'S':
+            break
+        d += dd[grid[y][x]]
+    return (abs(area) - edge)//2 + 1
 
 if __name__ == '__main__':
     aoc.main()
--- day11.py
+++ day11.py
@@ -2,28 +2,41 @@ import aoc
 
 @aoc.puzzle()
 def part1(inp):
-    return solve(inp, 2)
+    return distance_sum(inp.splitlines(), 2)
 
 @aoc.puzzle()
 def part2(inp):
-    return solve(inp, 1000000)
+    return distance_sum(inp.splitlines(), 1000000)
 
-def solve(inp, factor):
-    grid = inp.splitlines()
-    return total_distance(grid, factor) + total_distance(zip(*grid), factor)
+def distance_sum(grid, factor):
+    return distance_sum_major(grid, factor) + distance_sum_major(zip(*grid), factor)
 
-def total_distance(rows, factor):
+#TODO: can this be cleaner?
+def distance_sum_major(grid, factor):
     N = 0
     S1 = 0
     S2 = 0
     y = 0
-    for row in rows:
+    for row in grid:
         n = row.count('#')
         N += n
         S1 += (2*N - n)*n*y
         S2 += n*y
-        y += factor**(n==0)
+        y += factor if n == 0 else 1
     return S1 - N*S2
+
+def distance_sum_major_old(rows, factor):
+    ys = []
+    y = 0
+    for row in rows:
+        n = row.count('#')
+        ys.append((y, n))
+        y += factor if n == 0 else 1
+    distance = 0
+    for i, (y1, n1) in enumerate(ys):
+        for (y2, n2) in ys[i+1:]:
+            distance += n1*n2*(y2 - y1)
+    return distance
 
 if __name__ == '__main__':
     aoc.main()
