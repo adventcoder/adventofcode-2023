f4b74c331b37f35b94e6a6a0d562747ab61dd8a5
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-15 19:58:23 +0000

    remove duplication

--- day14.py
+++ day14.py
@@ -3,14 +3,15 @@ import click
 
 @aoc.puzzle()
 def part1(inp):
-    grid = [list(line) for line in inp.splitlines()]
-    tilt_north(grid)
-    return north_load(grid)
+    grid = parse_grid(inp)
+    rotate_clockwise(grid)
+    tilt_east(grid)
+    return load_east(grid)
 
 @aoc.puzzle()
 @click.option('--verbose', is_flag=True)
 def part2(inp, verbose):
-    grid = [list(line) for line in inp.splitlines()]
+    grid = parse_grid(inp)
     seen = {}
     cycles = 0
     target = 1000000000
@@ -28,58 +29,37 @@ def part2(inp, verbose):
     while cycles < target:
         cycle(grid)
         cycles += 1
-    return north_load(grid)
+    rotate_clockwise(grid)
+    return load_east(grid)
+
+def parse_grid(inp):
+    return [list(line) for line in inp.splitlines()]
 
 def make_key(grid):
     return bytes(c == 'O' for row in grid for c in row if c != '#')
 
-def north_load(grid):
-    return sum((len(grid) - y)*row.count('O') for y, row in enumerate(grid))
-
 def cycle(grid):
-    tilt_north(grid)
-    tilt_west(grid)
-    tilt_south(grid)
-    tilt_east(grid)
+    for _ in range(4):
+        rotate_clockwise(grid)
+        tilt_east(grid)
 
-def tilt_north(grid):
-    for x in range(len(grid[0])):
-        n = 0
-        for y in reversed(range(len(grid))):
-            if grid[y][x] == 'O':
-                n += 1
-            elif grid[y][x] == '#':
-                n = 0
-            else:
-                if n > 0:
-                    grid[y][x] = 'O'
-                    grid[y+n][x] = '.'
+def rotate_clockwise(grid):
+    transpose(grid)
+    flip(grid)
 
-def tilt_west(grid):
+def rotate_anticlockwise(grid):
+    flip(grid)
+    transpose(grid)
+
+def transpose(grid):
+    grid[:] = map(list, zip(*grid))
+
+def flip(grid):
     for row in grid:
-        n = 0
-        for x in reversed(range(len(row))):
-            if row[x] == 'O':
-                n += 1
-            elif row[x] == '#':
-                n = 0
-            else:
-                if n > 0:
-                    row[x] = 'O'
-                    row[x+n] = '.'
+        row.reverse()
 
-def tilt_south(grid):
-    for x in range(len(grid[0])):
-        n = 0
-        for y in range(len(grid)):
-            if grid[y][x] == 'O':
-                n += 1
-            elif grid[y][x] == '#':
-                n = 0
-            else:
-                if n > 0:
-                    grid[y][x] = 'O'
-                    grid[y-n][x] = '.'
+def load_east(grid):
+    return sum(x+1 for row in grid for x, c in enumerate(row) if c == 'O')
 
 def tilt_east(grid):
     for row in grid:
