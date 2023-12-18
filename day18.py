20e67b93d74189c0a31642954b92e25feb962ff6
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-18 20:15:37 +0000

    blerp

--- day18.py
+++ day18.py
@@ -9,18 +9,24 @@ def part1(inp):
 
 @aoc.puzzle()
 def part2(inp):
-    return area((code & 0xF, code >> 4, 0) for _, _, code in plan(inp))
+    return area(correct_plan(inp))
 
 def plan(inp):
     for line in inp.splitlines():
         args = line.split()
-        yield 'RDLU'.index(args[0]), int(args[1]), int(args[2][2:-1], 16)
+        yield 'RDLU'.index(args[0]), int(args[1])
+
+def correct_plan(inp):
+    for line in inp.splitlines():
+        args = line.split()
+        hex = args[2][2:-1]
+        yield int(hex[-1:], 16), int(hex[:-1], 16)
 
 def area(plan):
     edge = 0 # area of exterior
     area = 0 # 2*(area of the interior + area of inner half of the exterior)
     x, y = 0, 0
-    for d, n, _ in plan:
+    for d, n in plan:
         px, x = x, x + n*dx[d]
         py, y = y, y + n*dy[d]
         area += px*y - x*py
