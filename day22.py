da3471294d0163ed06bda6be050d446a908b0763
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-22 23:10:47 +0000

    cleaner but also slower

--- day22.py
+++ day22.py
@@ -1,37 +1,22 @@
 import aoc
 from dataclasses import dataclass
-from collections import deque
-
-#TODO: overengineered, could be simplified?
+from collections import defaultdict, deque
 
 @aoc.puzzle()
 def part1(inp):
-    stack = settle(inp)
-    return sum(stack.safe(i) for i in stack.lines())
+    tower = Tower(inp)
+    return sum(tower.safe(brick) for brick in tower.bricks)
 
 @aoc.puzzle()
 def part2(inp):
-    stack = settle(inp)
-    return sum(stack.pull(i) for i in stack.lines())
+    tower = Tower(inp)
+    return sum(tower.destroy(brick) for brick in tower.bricks)
 
-@dataclass
+@dataclass(frozen=True)
 class Brick:
     xs: range
     ys: range
     zs: range
-
-    def move(self, z):
-        return Brick(self.xs, self.ys, range(z, z + len(self.zs)))
-
-def settle(inp):
-    stack = Stack()
-    bricks = []
-    for line in inp.splitlines():
-        bricks.append(parse_brick(line))
-    bricks.sort(key=lambda b: b.zs.start)
-    for brick in bricks:
-        stack.drop(brick)
-    return stack
 
 def parse_brick(line):
     s1, s2 = line.split('~')
@@ -42,74 +27,54 @@ def parse_brick(line):
     zs = range(min(z1, z2), max(z1, z2) + 1)
     return Brick(xs, ys, zs)
 
-class Stack:
-    def __init__(self):
-        self.settled = []
-        self.above = []
-        self.below = []
+class Tower:
+    def __init__(self, inp):
+        self.bricks = []
+        self.layers = defaultdict(dict)
+        for brick in sorted(map(parse_brick, inp.splitlines()), key=lambda brick: brick.zs.start):
+            self.drop(brick)
 
     def drop(self, brick):
-        stop_z = 1
-        supports = []
-        for i, support in enumerate(self.settled):
-            if overlaps(brick.xs, support.xs) and overlaps(brick.ys, support.ys):
-                assert brick.zs.start >= support.zs.stop
-                if support.zs.stop > stop_z:
-                    supports = [i]
-                    stop_z = support.zs.stop
-                elif support.zs.stop == stop_z:
-                    supports.append(i)
-        j = len(self.settled)
-        self.settled.append(brick.move(stop_z))
-        self.below.append(supports)
-        self.above.append([])
-        for i in supports:
-            self.above[i].append(j)
+        start_z = brick.zs.start
+        while start_z > 1 and not self.intersect(brick.xs, brick.ys, start_z - 1):
+            start_z -= 1
+        zs = range(start_z, start_z + len(brick.zs))
+        for z in zs:
+            for x in brick.xs:
+                for y in brick.ys:
+                    self.layers[z][(x, y)] = len(self.bricks)
+        self.bricks.append(Brick(brick.xs, brick.ys, zs))
 
-    def lines(self):
-        return range(len(self.settled))
+    def intersect(self, xs, ys, z):
+        bricks = set()
+        for x in xs:
+            for y in ys:
+                if (x, y) in self.layers[z]:
+                    i = self.layers[z][(x, y)]
+                    bricks.add(self.bricks[i])
+        return bricks
 
-    def safe(self, i):
-        return all(self.supported(j, {i}) for j in self.above[i])
+    def below(self, brick):
+        return self.intersect(brick.xs, brick.ys, brick.zs.start - 1)
 
-    def pull(self, i):
-        removed = set()
-        queue = deque()
-        removed.add(i)
-        queue.extend(self.above[i])
+    def above(self, brick):
+        return self.intersect(brick.xs, brick.ys, brick.zs.stop)
+
+    def settled(self, brick, destroyed):
+        return len(self.below(brick) - destroyed) > 0
+
+    def safe(self, brick):
+        return all(self.settled(b, { brick }) for b in self.above(brick))
+
+    def destroy(self, brick):
+        destroyed = { brick }
+        queue = deque(self.above(brick))
         while queue:
-            i = queue.popleft()
-            if i not in removed and not self.supported(i, removed):
-                removed.add(i)
-                queue.extend(self.above[i])
-        return len(removed) - 1
-
-    def supported(self, i, removed):
-        return any(j not in removed for j in self.below[i])
-
-def overlaps(r1, r2):
-    return r1.start < r2.stop and r2.start < r1.stop
-
-def print_sideviews(bricks):
-    x_stop = max(b.xs.stop for b in bricks)
-    y_stop = max(b.ys.stop for b in bricks)
-    z_stop = max(b.zs.stop for b in bricks)
-    x_grid = [['.'] * x_stop for _ in range(z_stop)]
-    y_grid = [['.'] * x_stop for _ in range(z_stop)]
-    for i, b in enumerate(bricks):
-        c = chr(ord('A') + i)
-        for z in b.zs:
-            for x in b.xs:
-                x_grid[z][x] = c if x_grid[z][x] == '.' else '?'
-            for y in b.ys:
-                y_grid[z][y] = c if y_grid[z][y] == '.' else '?'
-    for x in range(x_stop):
-        x_grid[0][x] = '-'
-    for y in range(y_stop):
-        y_grid[0][y] = '-'
-    print('x'.center(x_stop), ' ', 'y'.center(y_stop))
-    for z in reversed(range(z_stop)):
-        print(''.join(x_grid[z]), ' ', ''.join(y_grid[z]), z)
+            brick = queue.popleft()
+            if brick not in destroyed and not self.settled(brick, destroyed):
+                destroyed.add(brick)
+                queue.extend(self.above(brick))
+        return len(destroyed) - 1
 
 if __name__ == '__main__':
     aoc.main()
