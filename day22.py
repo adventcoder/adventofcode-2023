9ff4d105bebedbd2aba2a9473ee74440f73f11b2
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-23 00:57:03 +0000

    washed

--- day22.py
+++ day22.py
@@ -1,79 +1,83 @@
 import aoc
-from dataclasses import dataclass
-from collections import defaultdict, deque
+from collections import deque, namedtuple
+
+Brick = namedtuple('Brick', ('xs', 'ys', 'zs'))
 
 @aoc.puzzle()
 def part1(inp):
-    tower = Tower(inp)
-    return sum(tower.safe(brick) for brick in tower.bricks)
+    bricks = parse_bricks(inp)
+    graph = build_graph(bricks)
+    return sum(graph.safe(i) for i in range(len(bricks)))
 
 @aoc.puzzle()
 def part2(inp):
-    tower = Tower(inp)
-    return sum(tower.destroy(brick) for brick in tower.bricks)
+    bricks = parse_bricks(inp)
+    graph = build_graph(bricks)
+    return sum(graph.chain_size(i) for i in range(len(bricks)))
 
-@dataclass(frozen=True)
-class Brick:
-    xs: range
-    ys: range
-    zs: range
+def parse_bricks(inp):
+    bricks = []
+    for line in inp.splitlines():
+        s1, s2 = line.split('~')
+        x1, y1, z1 = map(int, s1.split(','))
+        x2, y2, z2 = map(int, s2.split(','))
+        xs = range(min(x1, x2), max(x1, x2) + 1)
+        ys = range(min(y1, y2), max(y1, y2) + 1)
+        zs = range(min(z1, z2), max(z1, z2) + 1)
+        bricks.append(Brick(xs, ys, zs))
+    bricks.sort(key=lambda brick: brick.zs.start)
+    return bricks
 
-def parse_brick(line):
-    s1, s2 = line.split('~')
-    x1, y1, z1 = map(int, s1.split(','))
-    x2, y2, z2 = map(int, s2.split(','))
-    xs = range(min(x1, x2), max(x1, x2) + 1)
-    ys = range(min(y1, y2), max(y1, y2) + 1)
-    zs = range(min(z1, z2), max(z1, z2) + 1)
-    return Brick(xs, ys, zs)
-
-class Tower:
-    def __init__(self, inp):
-        self.bricks = []
-        self.layers = defaultdict(dict)
-        for brick in sorted(map(parse_brick, inp.splitlines()), key=lambda brick: brick.zs.start):
-            self.drop(brick)
-
-    def drop(self, brick):
-        start_z = brick.zs.start
-        while start_z > 1 and not self.intersect(brick.xs, brick.ys, start_z - 1):
+def build_graph(bricks):
+    tower = [{}] # floor
+    above = []
+    below = []
+    for i, brick in enumerate(bricks):
+        # Find how far the brick drops
+        start_z = len(tower)
+        while start_z > 1 and all((x, y) not in tower[start_z - 1] for x in brick.xs for y in brick.ys):
             start_z -= 1
-        zs = range(start_z, start_z + len(brick.zs))
-        for z in zs:
+        stop_z = start_z + len(brick.zs)
+
+        # Add the brick to the tower
+        while len(tower) < stop_z:
+            tower.append({})
+        for z in range(start_z, stop_z):
             for x in brick.xs:
                 for y in brick.ys:
-                    self.layers[z][(x, y)] = len(self.bricks)
-        self.bricks.append(Brick(brick.xs, brick.ys, zs))
+                    tower[z][(x, y)] = i
 
-    def intersect(self, xs, ys, z):
-        bricks = set()
-        for x in xs:
-            for y in ys:
-                if (x, y) in self.layers[z]:
-                    i = self.layers[z][(x, y)]
-                    bricks.add(self.bricks[i])
-        return bricks
+        # Add the brick to the graph
+        above.append(set())
+        below.append(set())
+        for x in brick.xs:
+            for y in brick.ys:
+                if (x, y) in tower[start_z - 1]:
+                    j = tower[start_z - 1][(x, y)]
+                    below[i].add(j)
+                    above[j].add(i)
 
-    def below(self, brick):
-        return self.intersect(brick.xs, brick.ys, brick.zs.start - 1)
+    return Graph(above, below)
 
-    def above(self, brick):
-        return self.intersect(brick.xs, brick.ys, brick.zs.stop)
+class Graph:
+    def __init__(self, above, below):
+        self.above = above
+        self.below = below
 
-    def settled(self, brick, destroyed):
-        return len(self.below(brick) - destroyed) > 0
+    def settled(self, i, destroyed):
+        return len(self.below[i] - destroyed) > 0
 
-    def safe(self, brick):
-        return all(self.settled(b, { brick }) for b in self.above(brick))
+    def safe(self, i):
+        return all(self.settled(j, { i }) for j in self.above[i])
 
-    def destroy(self, brick):
-        destroyed = { brick }
-        queue = deque(self.above(brick))
+    def chain_size(self, i):
+        destroyed = { i }
+        queue = deque(self.above[i])
         while queue:
-            brick = queue.popleft()
-            if brick not in destroyed and not self.settled(brick, destroyed):
-                destroyed.add(brick)
-                queue.extend(self.above(brick))
+            i = queue.popleft()
+            if i not in destroyed and not self.settled(i, destroyed):
+                destroyed.add(i)
+                queue.extend(self.above[i])
         return len(destroyed) - 1
 
 if __name__ == '__main__':
