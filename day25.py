0d24294734d6741448090ba7f0745bc1f640f415
adventcoder <59347361+adventcoder@users.noreply.github.com>
2023-12-29 13:18:42 +0000

    done for the year

--- day25.py
+++ day25.py
@@ -1,55 +1,63 @@
 import aoc
-from collections import defaultdict, deque
+from collections import defaultdict, Counter
+from math import inf
 
-#TODO: actually solve
+#TODO: this could be optimised...
 
 @aoc.puzzle()
 def part1(inp):
-    graph = defaultdict(set)
+    edges = defaultdict(Counter)
     for line in inp.splitlines():
-        name, connected = line.split(': ')
-        for output in connected.split():
-            graph[name].add(output)
-            graph[output].add(name)
+        a, bs = line.split(':')
+        for b in bs.split():
+            edges[a][b] = 1
+            edges[b][a] = 1
+    total = len(edges)
+    cut, partition = find_minimum_cut(edges)
+    assert cut == 3
+    return len(partition) * (total - len(partition))
 
-    # dot -Tpng -o day25.png -Kneato day25.dot
-    with open('day25.dot', 'w') as file:
-        print('graph {', file=file)
-        seen = set()
-        for name in graph:
-            print(f'{name};', file=file)
-            for other in graph[name]:
-                if other not in seen:
-                    print(f'{name} -- {other};', file=file)
-            seen.add(name)
-        print('}', file=file)
+def find_minimum_cut(edges):
+    min_cut = inf
+    min_t = None
+    while len(edges) > 1:
+        s, t, cut = find_minimum_st_cut(edges)
+        if cut < min_cut:
+            min_cut = cut
+            min_t = t
+        contract(edges, s, t)
+    return min_cut, min_t.split(',')
 
-    # By inspection :)
-    cut(graph, 'ddl/lcm')
-    cut(graph, 'qnd/mbk')
-    cut(graph, 'rrl/pcs')
-
-    island1 = flood(graph, 'ddl')
-    island2 = flood(graph, 'lcm')
-    return len(island1) * len(island2)
-
-def cut(graph, edge):
-    a, b = edge.split('/')
-    graph[a].remove(b)
-    graph[b].remove(a)
-
-def flood(graph, name):
-    queue = deque()
-    seen = set()
-    queue.append(name)
-    seen.add(name)
+# Find any minimum s-t cut in the graph (using maximum adjacency search)
+# t is a single super vertex representing the sink partition
+# s is an arbitrary vertex from the source partition which makes up the rest of the graph
+def find_minimum_st_cut(edges):
+    # queue ordered by combined weight of edges to the visited set
+    queue = { v: 0 for v in edges.keys() }
+    t = None
     while queue:
-        name = queue.popleft()
-        for output in graph[name]:
-            if output not in seen:
-                queue.append(output)
-                seen.add(output)
-    return seen
+        # Pop the vertex with the most combined edge weight
+        s = t
+        t = max(queue, key=queue.get)
+        cut = queue.pop(t)
+        # Add it to the visited set
+        for n, w in edges[t].items():
+            if n in queue:
+                queue[n] += w
+    return s, t, cut
+
+def contract(edges, a, b):
+    c = ','.join([a, b])
+    for n in edges[a]:
+        if n != b:
+            edges[c][n] += edges[n].pop(a)
+    for n in edges[b]:
+        if n != a:
+            edges[c][n] += edges[n].pop(b)
+    for n in edges[c]:
+        edges[n][c] = edges[c][n]
+    edges.pop(a)
+    edges.pop(b)
 
 if __name__ == '__main__':
     aoc.main()
